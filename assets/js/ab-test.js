adf22bca4a20d76a467b80f0c5e72467ac326c7b
jorisstrix <joris.van.os@strix.net>
2024-11-08 19:58:53 +0100

    split_script

--- assets/js/ab-test.js
+++ assets/js/ab-test.js
@@ -1,16 +1,25 @@
-window.dataLayer = window.dataLayer || [];
 (function() {
-  const weightForA = 50;
-  const RANDOM_MAX = 2147483648;
-  const COOKIE_EXPIRATION_DAYS = 30;
-  const COOKIE_MAX_AGE = 60 * 60 * 24 * COOKIE_EXPIRATION_DAYS;
-  const randomNumber = Math.floor(Math.random() * RANDOM_MAX);
-  const variant = (randomNumber % 100) < weightForA ? 'A' : 'B';
-  window.dataLayer.push({
-    event: 'abTestPerformance',
-    testName: 'AB test name here',
+  const cookie_expiration_days = 30;
+  const cookie_max_age = 60 * 60 * 24 * cookie_expiration_days;
+  const weight_for_a = 50;
+  const random_max = 2147483648;
+  const random_number = Math.floor(Math.random() * random_max);
+  const variant = (random_number % 100) < weight_for_a ? 'a' : 'b';
+  
+  document.cookie = `strixab=${variant}; path=/; max-age=${cookie_max_age}`;
+})();
+
+(function() {
+  const variant = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('strixab='))
+    ?.split('=')[1] || 'b';
+
+  window.datalayer = window.datalayer || [];
+  window.datalayer.push({
+    event: 'abtestperformance',
+    testname: 'ab test name here',
     variant: variant,
     timestamp: new Date().toISOString()
   });
-  document.cookie = `strixAB=${variant}; path=/; max-age=${COOKIE_MAX_AGE}`;
 })();
