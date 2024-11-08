9e05b2721365414881e5af5c6a3f77aca7bee3ca
jorisstrix <joris.van.os@strix.net>
2024-11-08 20:53:56 +0100

    again

--- assets/js/ab-test.js
+++ assets/js/ab-test.js
@@ -1,12 +1,9 @@
-// ab-test-datalayer.js
 (function() {
-  console.log("Loading datalayer script");
-  window.datalayer = window.datalayer || [];
-  window.datalayer.push({
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
     event: 'abtestperformance',
     testname: 'ab test name here',
     variant: 'a',
     timestamp: new Date().toISOString()
   });
-  console.log("Event pushed to datalayer:", window.datalayer);
 })();
