c9509f828ed8fe1ca4776f66d13b1ae7f8fc35cd
jorisstrix <joris.van.os@strix.net>
2024-11-08 20:33:16 +0100

    loggers

--- assets/js/ab-test.js
+++ assets/js/ab-test.js
@@ -1,8 +1,15 @@
+// ab-test.js
 (function() {
   function load_script(src) {
     const script = document.createElement('script');
     script.src = src;
     script.defer = true;
+    script.onload = function() {
+      console.log(src + ' loaded successfully');
+    };
+    script.onerror = function() {
+      console.log('Error loading ' + src);
+    };
     document.head.appendChild(script);
   }
 
