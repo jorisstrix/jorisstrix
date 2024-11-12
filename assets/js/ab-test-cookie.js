26fc6b873614a63d04d20d1cc945121187b32b95
jorisstrix <joris.van.os@strix.net>
2024-11-12 14:58:36 +0100

    ab-testJS

--- assets/js/ab-test-cookie.js
+++ assets/js/ab-test-cookie.js
@@ -1,10 +1,10 @@
 (function() {
-    const cookie_expiration_days = 30;
-    const cookie_max_age = 60 * 60 * 24 * cookie_expiration_days;
-    const weight_for_a = 50;
-    const random_max = 2147483648;
-    const random_number = Math.floor(Math.random() * random_max);
-    const variant = (random_number % 100) < weight_for_a ? 'a' : 'b';
-    document.cookie = `strixab=${variant}; path=/; max-age=${cookie_max_age}`;
-  })();  
+    const cookieExpirationDays = 30;
+    const cookieMaxAge = 60 * 60 * 24 * cookieExpirationDays;
+    const weightForA = 50;
+    const randomMax = 2147483648;
+    const randomNumber = Math.floor(Math.random() * randomMax);
+    const variant = (randomNumber % 100) < weightForA ? 'A' : 'B';
+    document.cookie = `StrixAB=${variant}; path=/; max-age=${cookieMaxAge}`;
+})();
   
