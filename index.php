cabeb240343d61fdc65444edbbaac6f313897ced
jorisstrix <joris.van.os@strix.net>
2024-11-14 16:04:45 +0100

    Twig formatter and rebuild folder structure of twigTemplates

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'vendor/autoload.php'; // Load Composer's autoloader
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new \Twig\Loader\FilesystemLoader('twigTemplates');
 $twig = new \Twig\Environment($loader);
 
 echo $twig->render('index.html.twig'); // Render the main template
